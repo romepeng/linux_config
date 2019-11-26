f6402f1340fed6622db305b1150322b7260b2fa3
romepeng <romepeng@outlook.com>
2019-11-26 15:29:51 +0800

    add  tsplus files.

---
+++ myapp.py
@@ -0,0 +1,8 @@
+#!/usr/bin/pyhton
+def app(environ, start_response):
+        data = b"Hello, World!\n"
+        start_response("200 OK", [
+            ("Content-Type", "text/plain"),
+            ("Content-Length", str(len(data)))
+        ])
+        return iter([data])
